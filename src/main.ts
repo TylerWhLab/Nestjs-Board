784e76ffd044b5f6e4d0864cfd275d7ae6568ddd
unknown <tylerdurden.wh@gmail.com>
2022-02-23 21:59:40 +0900

    update config

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as config from 'config';
 
-
 // entry point
 async function bootstrap() {
   const app = await NestFactory.create(AppModule); // app 생성, AppModule이 root module
@@ -12,7 +11,6 @@ async function bootstrap() {
   const port = serverConf.port;
   
   await app.listen(port); // app 실행, port 지정
-  // console.log(`Nest Start ${port}`);
   Logger.log(`Application running on port ${port}`);
 }
 bootstrap();
